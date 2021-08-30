5974b29dea7a5450520c5cefc2d19cdbd167ac9c
Jodi Chapson <2152594@students.wits.ac.za>
2021-08-30 05:12:09 +0200

    isometric movement implementation ;)

--- Communism Simulator/Assets/Scripts/Player.cs
+++ Communism Simulator/Assets/Scripts/Player.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public float movespeed = 3f;
+    public float speedMod = 0.5f;
+
+    Vector3 forward, right;
+
+    public void Start()
     {
-        
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+
+
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (Input.anyKey)
+        {
+            Move();
+        }
     }
+
+    public void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
+        Vector3 rightMove;
+        Vector3 upMove;
+
+        if (direction.x != 0 && direction.z != 0)
+        {
+            //character is going diagonal
+            rightMove = right * movespeed * speedMod * Time.deltaTime * Input.GetAxis("HorizontalKey");
+            upMove = forward * movespeed * speedMod * Time.deltaTime * Input.GetAxis("VerticalKey");
+        }
+        else
+        {
+            rightMove = right * movespeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+            upMove = forward * movespeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+        }
+
+        
+
+        Vector3 heading = Vector3.Normalize(rightMove + upMove);
+        
+        //rotates character
+        transform.forward = heading;
+        //moves character
+        transform.position += rightMove;
+        transform.position += upMove;
+    }
+
+
 }
